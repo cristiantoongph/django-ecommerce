3cae6fd1e4ba1ba8472044b484cfa4fd264416fb
cristian to-ong <cristiantoong@gmail.com>
2023-08-28 12:53:18 +0800

    final commit

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -13,6 +13,18 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
   product = Product.objects.get(id=product_id)#get product
+  product_variation = []
+  if request.method == 'POST':
+    #loop tru variations(color and size)
+    for item in request.POST:
+      key = item
+      value = request.POST[key]
+      # check if key and value matches Variation Model methods (color and size)
+      try:
+        variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+        product_variation.append(variation)
+      except:
+        pass
 
   #if cart exists get the card in session else create cart object
   try:
